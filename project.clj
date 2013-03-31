4d03a53b7eaf6e25e18f289f18acbd83c25ae9ed
Dean Reilly <me@deanreilly.co.uk>
2013-03-31 14:18:53 +0100

    Initial commit.

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject eighteen-point-problem "0.1.0-SNAPSHOT"
+  :description "Derives all solutions to the 18 point problem."
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"] [org.clojure/core.logic "0.8.3"]])
---
+++ test/eighteen_point_problem/core_test.clj
@@ -0,0 +1,7 @@
+(ns eighteen-point-problem.core-test
+  (:use clojure.test
+        eighteen-point-problem.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
